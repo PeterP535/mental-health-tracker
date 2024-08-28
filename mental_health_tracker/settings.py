96022886ade740d39a7ebfaa2f612a8bdfd8b5a3
PeterP535 <peterputra1@gmail.com>
2024-08-28 14:28:32 +0700

    dd

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9(3(xd4l65-twwwa16^qal69uwrp&h2!&)x02hcdv_^hfs1g_+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "peter-putra-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 
 # Application definition
